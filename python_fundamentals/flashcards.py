789e79edcd03cfbdbea811a5a79d13b5c0950bd1
muaddib576 <36249678+muaddib576@users.noreply.github.com>
2021-06-15 18:07:53 -0700

    pretify flashcards

--- python_fundamentals/flashcards.py
+++ python_fundamentals/flashcards.py
@@ -1,5 +1,12 @@
 from pathlib import Path
+from datetime import datetime
 import random
+import shutil
+
+
+
+terminal_size = shutil.get_terminal_size()
+width = terminal_size[0]
 
 def load_csv(path):
     """does a specific thing"""
@@ -33,24 +40,52 @@ def load_csv(path):
 
     return card_list
 
+def scorekeeper(path, correct, total):
+    """logs the results in score_log.csv"""
+    
+    incorrect = total - correct
+    dt = datetime.today()
+    ts = dt.timestamp()
+    ts = int(ts)
+
+    if not path.exists():
+        print(f"ERROR: {path} does not exist")
+        return 
+
+    with open(path, 'a') as ch:
+         ch.write(f"{ts},{correct},{incorrect}\n")
+
+    print("This outcome has been recorded for posterity.")
+
+
+
 def play(play_cards):
     """Takes the formatted cards and challenges the player"""
 
     score = 0
     denom = len(play_cards)
-    i = 1
+    i = 0
 
     while play_cards:
         
         card = random.choice(play_cards)
         play_cards.remove(card)
-
-        print("=" * 20)
-        print(f"Flashcard {i} of {denom}")
         i += 1
-        print("=" * 20)
 
-        answer = input(f"Hey there Dummy, here's an easy one: \nHow do you {card['front']}?\nAnswer: ")
+        print()
+        print("=" * width)
+        print("|", end = "")
+        print(f"Flashcard {i} of {denom}".center(width-2), end = "")
+        print("|")
+        print("|", end = "")
+        print("Hey there Dummy, here's an easy one:".center(width-2), end = "")
+        print("|")
+        print("|", end = "")
+        print(f"How do you {card['front']}?".center(width-2), end = "")
+        print("|")
+        print("=" * width)
+
+        answer = input("\nAnswer: ")
 
         if answer == card["back"]:
             print("Not as dumb as you look... CORRECT")
@@ -58,15 +93,27 @@ def play(play_cards):
             continue
         
         print(f"Whoops. Sorry I was looking for: {card['back']}")
+        print()
 
-    print("=" * 20)
-    print(f"Final Score: {score} out of {denom}")
-    print("=" * 20)
+    print("Game Over".center(width))
+
+    print(".-=========-.".center(width))
+    print("('-=======-')".center(width))
+    print("_|   .=.   |_".center(width))
+    print(f"((|  {score} of {denom} |))".center(width))
+    print("\|   /|\   |/".center(width))
+    print("\__ '`' __/".center(width))
+    print("_`) (`_".center(width))
+    print("_/_______\_".center(width))
+    print("(___________)".center(width))
+
+    return score, denom
 
 def main():
     """does the main thing"""
 
     card_path = Path.cwd() / "data/flashcards/paths.csv"
+    log_path = Path.cwd() / "data/flashcards/score_log.csv"
 
     cards = load_csv(card_path)
 
@@ -74,6 +121,8 @@ def main():
         print("Error: someth went wrong.")
         return
     
-    play(cards)
+    results = play(cards)
+
+    scorekeeper(log_path, results[0], results[1])
 
 main()
