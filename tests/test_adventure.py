6a1087504075cb862ce6384db01bfa4513994890
muaddib576 <36249678+muaddib576@users.noreply.github.com>
2022-04-05 18:32:37 -0700

    adding tests

--- tests/test_adventure.py
+++ tests/test_adventure.py
@@ -1,18 +1,73 @@
+from copy import deepcopy
 import pytest
-from python_fundamentals.adventure import (Go, error)
+import python_fundamentals.adventure as adventure
+from python_fundamentals.adventure import (
+    Go,
+    Command,
+    error,
+    debug,
+    header,
+    write,
+    )
+
+PLAYER_STATE = deepcopy(adventure.PLAYER)
+PLACES_STATE = deepcopy(adventure.PLACES)
+ITEMS_STATE = deepcopy(adventure.ITEMS)
+MARGIN_STATE = deepcopy(adventure.MARGIN)
 
 
+def revert():
+    """Revert game data to its original state."""
+    adventure.PLAYER = deepcopy(PLAYER_STATE)
+    adventure.PLACES = deepcopy(PLACES_STATE)
+    adventure.ITEMS = deepcopy(ITEMS_STATE)
+    adventure.MARGIN = deepcopy(MARGIN_STATE)
+
+
+@pytest.fixture(autouse=True)
+def teardown(request):
+    """Auto-add teardown method to all tests."""
+    request.addfinalizer(revert)
 
 def test_error(capsys):
-    error("test1")
+    error("error test")
     output = capsys.readouterr().out
     
-    assert output == "Error: test1\n"
+    assert output == "Error: error test\n"
+
+def test_debug(capsys):
+    debug("debug test")
+    output = capsys.readouterr().out
+
+    assert output == f"!!! debug test\n"
+
+def test_header(capsys):
+    header("header test")
+    output = capsys.readouterr().out
+
+    assert output == f"\n{adventure.MARGIN}header test\n\n"
+
+def test_write(capsys):
+    write("write test")
+    output = capsys.readouterr().out
+
+    assert output == f"{adventure.MARGIN}write test\n"
 
 def test_go(capsys):
     Go(['east']).do()
     output = capsys.readouterr().out
-    # print(output)
+
     assert "The square part of town." in output
 
-# test_go()
+# split test_place into two functions
+def test_get_place(capsys):
+    
+    assert Command('args').get_place() == adventure.PLACES[adventure.PLAYER['place']], \
+        "Starting location should be 'your cottage'"
+    
+    adventure.PLAYER['place'] = 'shire'
+    Command('args').get_place()
+    output = capsys.readouterr().out
+
+    assert output == "Error: It seems the player exists outside the known universe...\n", \
+        "A location not in Places should throw an error"
